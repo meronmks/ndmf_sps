598292087bcaa29308bbda48fd89f163d1430aa2
meronmks <meronmks.8914@gmail.com>
2024-09-02 23:26:11 +0900

    SocketのInspector拡張をちょっと記載

--- Assets/ndmf_sps/Editor/CustomEditor/SocketEditor.cs
+++ Assets/ndmf_sps/Editor/CustomEditor/SocketEditor.cs
@@ -1,11 +1,23 @@
-﻿using com.meronmks.ndmfsps.runtime;
+﻿using nadena.dev.modular_avatar.core;
 using UnityEditor;
+using UnityEngine;
 
 namespace com.meronmks.ndmfsps
 {
+    using runtime;
+    
     [CustomEditor(typeof(Socket))]
     internal class SocketEditor : Editor
     {
-        
+        public override void OnInspectorGUI()
+        {
+            Socket socket = target as Socket;
+            Localization.SelectLanguageGUI();
+            EditorGUILayout.Separator();
+            EditorGUILayout.LabelField(Localization.S("inspector.common.boneReference"));
+            socket.boneReference = (HumanBodyBones)EditorGUILayout.EnumPopup(socket.boneReference);
+            EditorGUILayout.LabelField(Localization.S("inspector.common.attachmentMode"));
+            socket.attachmentMode = (BoneProxyAttachmentMode)EditorGUILayout.EnumPopup(socket.attachmentMode);
+        }
     }
 }
