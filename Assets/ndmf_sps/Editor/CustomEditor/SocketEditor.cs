9333b2ffc2d5f98bbfd58fc354f689e1b2b2a89f
meronmks <meronmks.8914@gmail.com>
2024-09-03 20:50:18 +0900

    List部分以外のInspector拡張はほぼおわり

--- Assets/ndmf_sps/Editor/CustomEditor/SocketEditor.cs
+++ Assets/ndmf_sps/Editor/CustomEditor/SocketEditor.cs
@@ -11,13 +11,82 @@ namespace com.meronmks.ndmfsps
     {
         public override void OnInspectorGUI()
         {
+            serializedObject.UpdateIfRequiredOrScript();
             Socket socket = target as Socket;
             Localization.SelectLanguageGUI();
             EditorGUILayout.Separator();
-            EditorGUILayout.LabelField(Localization.S("inspector.common.boneReference"));
-            socket.boneReference = (HumanBodyBones)EditorGUILayout.EnumPopup(socket.boneReference);
-            EditorGUILayout.LabelField(Localization.S("inspector.common.attachmentMode"));
-            socket.attachmentMode = (BoneProxyAttachmentMode)EditorGUILayout.EnumPopup(socket.attachmentMode);
+            
+            var pBoneProxy = serializedObject.FindProperty(nameof(Socket.boneReference));
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(pBoneProxy, Localization.G("inspector.common.boneReference"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+            
+            var pAttachmentMode = serializedObject.FindProperty(nameof(Socket.attachmentMode));
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(pAttachmentMode, Localization.G("inspector.common.attachmentMode"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+            
+            var pEnableDepthAnimations = serializedObject.FindProperty(nameof(Socket.enableDepthAnimations));
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(pEnableDepthAnimations, Localization.G("inspector.common.enableDepthAnimations"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            if (pEnableDepthAnimations.boolValue)
+            {
+                var pDepthActions = serializedObject.FindProperty(nameof(Socket.depthActions));
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.PropertyField(pDepthActions, Localization.G("inspector.common.depthActions"));
+                if (EditorGUI.EndChangeCheck())
+                {
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+            
+            var pMode = serializedObject.FindProperty(nameof(Socket.mode));
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(pMode, Localization.G("inspector.socket.mode"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+            
+            var pEnableActiveAnimation = serializedObject.FindProperty(nameof(Socket.enableActiveAnimation));
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(pEnableActiveAnimation, Localization.G("inspector.socket.enableActiveAnimation"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+            
+            if (pEnableActiveAnimation.boolValue)
+            {
+                var pActiveAnimationActions = serializedObject.FindProperty(nameof(Socket.activeAnimationActions));
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.PropertyField(pActiveAnimationActions, Localization.G("inspector.socket.activeAnimationActions"));
+                if (EditorGUI.EndChangeCheck())
+                {
+                    serializedObject.ApplyModifiedProperties();
+                }
+            }
+            
+            var pHaptics = serializedObject.FindProperty(nameof(Socket.haptics));
+            EditorGUI.BeginChangeCheck();
+            EditorGUILayout.PropertyField(pHaptics, Localization.G("inspector.socket.haptics"));
+            if (EditorGUI.EndChangeCheck())
+            {
+                serializedObject.ApplyModifiedProperties();
+            }
+
+            serializedObject.ApplyModifiedProperties();
         }
     }
 }
