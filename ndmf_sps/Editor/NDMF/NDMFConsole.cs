f7ea8343b47547362432355acd3aded24fef172b
meronmks <meronmks.8914@gmail.com>
2024-09-09 00:18:51 +0900

    NDMFコンソールに何か出すときに使うヘルパー定義

---
+++ ndmf_sps/Editor/NDMF/NDMFConsole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using nadena.dev.ndmf;
+using nadena.dev.ndmf.localization;
+
+namespace com.meronmks.ndmfsps
+{
+    internal static class NDMFConsole
+    {
+        private static readonly Localizer Localizer = new Localizer("en-us", () =>
+        {
+            return Localization.GetCodes().Select(code => (code, LocalizationFunction(code))).ToList();
+        });
+        
+        private static Func<string, string> LocalizationFunction(string code)
+        {
+            return key => Localization.S(key, code);
+        }
+
+        public static void LogInfo(string key, params object[] args)
+        {
+            ErrorReport.ReportError(Localizer, ErrorSeverity.Information, key, args);
+        }
+        
+        public static void LogWarning(string key, params object[] args)
+        {
+            ErrorReport.ReportError(Localizer, ErrorSeverity.NonFatal, key, args);
+        }
+        
+        public static void LogError(string key, params object[] args)
+        {
+            ErrorReport.ReportError(Localizer, ErrorSeverity.Error, key, args);
+        }
+    }
+}
