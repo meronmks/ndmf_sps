98f5347452ad501b1acb409fe640ae5a50ee590f
meronmks <meronmks.8914@gmail.com>
2024-09-09 00:18:03 +0900

    汎用的に出すメッセージ用部品を定義

---
+++ ndmf_sps/Editor/CustomEditor/CommonGUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+
+namespace com.meronmks.ndmfsps
+{
+    internal static class CommonGUI
+    {
+        internal static void ShowCommonHelpBox()
+        {
+            EditorGUILayout.HelpBox(Localization.S("inspector.common.commonInfoMes"), MessageType.Info);
+        }
+    }
+}
